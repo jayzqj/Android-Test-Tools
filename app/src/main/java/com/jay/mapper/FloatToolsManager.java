aa96107a543457e200d7e24adb3cd7989b369de8
zengqingjie <zengqingjie@wanda.cn>
2017-04-19 17:38:04 +0800

    效果截图

--- app/src/main/java/com/jay/mapper/FloatToolsManager.java
+++ app/src/main/java/com/jay/mapper/FloatToolsManager.java
@@ -24,12 +24,12 @@ public class FloatToolsManager {
         serverData.add("https://www.12345.com");
         //配置功能测试项
         List<ActionBean> data = new ArrayList<>();
-        data.add(new ActionBean("二维码扫描1", MainActivity.class.getCanonicalName()));
         data.add(new ActionBean("二维码扫描", MainActivity.class.getCanonicalName()));
-        data.add(new ActionBean("二维码扫描", MainActivity.class.getCanonicalName()));
-        data.add(new ActionBean("二维码扫描", MainActivity.class.getCanonicalName()));
-        data.add(new ActionBean("二维码扫描", MainActivity.class.getCanonicalName()));
-        data.add(new ActionBean("二维码扫描3", MainActivity.class.getCanonicalName()));
+        data.add(new ActionBean("交易详情", MainActivity.class.getCanonicalName()));
+        data.add(new ActionBean("密码设置", MainActivity.class.getCanonicalName()));
+        data.add(new ActionBean("产品详情", MainActivity.class.getCanonicalName()));
+        data.add(new ActionBean("登录页面", MainActivity.class.getCanonicalName()));
+        data.add(new ActionBean("订单页面", MainActivity.class.getCanonicalName()));
         Config config = new Config.Build(serverData, data).build();
         TestTools.init(application, config);
         TestTools.setListener(new OperatorViewCallBack() {
